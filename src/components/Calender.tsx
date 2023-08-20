41fa828e4f8a4dd5bd9a95009cda59f1dee41f7a
tatata05 <takumi.takahata@xincere.jp>
2023-08-20 15:16:18 +0900

    管理者シフトカレンダー

---
+++ src/components/Calender.tsx
@@ -0,0 +1,39 @@
+import FullCalendar from "@fullcalendar/react";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+type shiftArray = {
+  rows: rows[];
+};
+
+type rows = {
+  key: number;
+  title: string;
+  start: Date;
+  end: Date;
+  description: string;
+  backgroundColor: string;
+  borderColor: string;
+};
+
+export default function Calendar({ rows }: shiftArray) {
+  return (
+    <>
+      <div>
+        {/* TODO : デザイン等調整 */}
+        <FullCalendar
+          plugins={[timeGridPlugin]}
+          initialView="timeGridWeek"
+          initialEvents={rows}
+          locale={jaLocale}
+          allDaySlot={false}
+          headerToolbar={{
+            left: "timeGridDay timeGridWeek",
+            center: "title",
+            right: "prev today next",
+          }}
+        />
+      </div>
+    </>
+  );
+}
