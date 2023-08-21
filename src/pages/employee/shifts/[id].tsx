c05e689febec513ca7a27231a4f58862ff261cf2
tatata05 <takumi.takahata@xincere.jp>
2023-08-21 22:33:37 +0900

    従業員/シフト詳細

---
+++ src/pages/employee/shifts/[id].tsx
@@ -0,0 +1,30 @@
+import ShiftShow from "@/components/ShiftAbsenceShow";
+import EmployeeHeader from "@/components/EmployeeHeader";
+import MyHead from "@/components/MyHead";
+import StatusButton from "@/components/StatusButton";
+
+export default function EmployeeShiftShow() {
+  // 仮の時間を設定
+  const startTime = new Date("5 17, 1998 18:00:00");
+  const endTime = new Date("5 17, 1998 23:00:00");
+  return (
+    <>
+      <MyHead title="シフト詳細" />
+      <EmployeeHeader />
+      <main className="text-center">
+        <h2 className="mt-5 mb-3">シフト詳細</h2>
+        {/* TODO : <p>該当するシフトは存在しません</p> */}
+        <ShiftShow
+          name="Me is 従業員"
+          startTime={startTime.toLocaleString()}
+          endTime={endTime.toLocaleString()}
+          status="未承認"
+        />
+        <div className="d-flex justify-content-center col-8 mx-auto text-center">
+          <StatusButton link="/" statusLabel="編集" />
+          <StatusButton link="/" statusLabel="取消" />
+        </div>
+      </main>
+    </>
+  );
+}
