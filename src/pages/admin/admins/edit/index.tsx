8f8c4c3ac3ad68d1e502b9190836fcd225e1a89c
tatata05 <takumi.takahata@xincere.jp>
2023-08-26 16:35:10 +0900

    管理者編集ページ

--- src/pages/admin/admins/edit/index.tsx
+++ src/pages/admin/admins/edit/index.tsx
@@ -1,13 +1,13 @@
 import AdminHeader from "@/components/AdminHeader";
 import MyHead from "@/components/MyHead";
 
-export default function AdminAdminsNew() {
+export default function AdminAdminsEdit() {
   return (
     <>
-      <MyHead title="管理者登録" />
+      <MyHead title="管理者編集" />
       <AdminHeader />
       <main className="text-center">
-        <h2 className="mt-5 mb-3">管理登録</h2>
+        <h2 className="mt-5 mb-3">管理者編集</h2>
         <form action="/" method="post">
           <div className="col-3 mx-auto">
             <label className="form-label">管理者名</label>
@@ -18,22 +18,29 @@ export default function AdminAdminsNew() {
             <input type="email" className="form-control"></input>
           </div>
           <div className="col-3 mx-auto mt-3">
-            <label className="form-label">パスワード</label>
+            <label className="form-label">新規パスワード</label>
             <input type="password" className="form-control"></input>
             <div id="passwordHelpBlock" className="form-text">
-              6文字以上
+              6文字以上・更新する場合のみ入力してください
             </div>
           </div>
           <div className="col-3 mx-auto mt-3">
-            <label className="form-label">パスワード(確認)</label>
+            <label className="form-label">新規パスワード(確認)</label>
             <input type="password" className="form-control"></input>
             <div id="passwordHelpBlock" className="form-text">
-              6文字以上
+              6文字以上・更新する場合のみ入力してください
+            </div>
+          </div>
+          <div className="col-3 mx-auto mt-3">
+            <label className="form-label">現在のパスワード</label>
+            <input type="password" className="form-control"></input>
+            <div id="passwordHelpBlock" className="form-text">
+              6文字以上・必須入力
             </div>
           </div>
           <div>
             <button type="submit" className="btn btn-primary mt-4">
-              管理者を登録する
+              編集を保存する
             </button>
           </div>
         </form>
