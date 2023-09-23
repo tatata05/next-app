6246e8812351823435577cb64c6575260edd9aba
tatata05 <takumi.takahata@xincere.jp>
2023-09-23 10:30:59 +0900

    fix: delete_extra_tag

--- src/components/From.tsx
+++ src/components/From.tsx
@@ -14,27 +14,25 @@ type FormProps = {
 
 export default function From({ action, submitLabel, rows }: FormArray) {
   return (
-    <>
-      <form action={action} method="post">
-        {rows.map((row, index) => (
-          <div className="col-3 mx-auto mt-3" key={index}>
-            <label className="form-label">{row.nameLabel}</label>
-            {row.type === "radio" ? (
-              <RadioButton />
-            ) : (
-              <>
-                <input type={row.type} className="form-control" />
-                <div className="form-text">{row.warningLabel}</div>
-              </>
-            )}
-          </div>
-        ))}
-        <div>
-          <button type="submit" className="btn btn-primary mt-4">
-            {submitLabel}
-          </button>
+    <form action={action} method="post">
+      {rows.map((row, index) => (
+        <div className="col-3 mx-auto mt-3" key={index}>
+          <label className="form-label">{row.nameLabel}</label>
+          {row.type === "radio" ? (
+            <RadioButton />
+          ) : (
+            <>
+              <input type={row.type} className="form-control" />
+              <div className="form-text">{row.warningLabel}</div>
+            </>
+          )}
         </div>
-      </form>
-    </>
+      ))}
+      <div>
+        <button type="submit" className="btn btn-primary mt-4">
+          {submitLabel}
+        </button>
+      </div>
+    </form>
   );
 }
