20c287d01de71770e2c58b889019eec84d2d4382
tatata05 <takumi.takahata@xincere.jp>
2023-12-05 17:46:52 +0900

    signIn_and_setCookies

--- src/components/From.tsx
+++ src/components/From.tsx
@@ -1,4 +1,7 @@
+import KintaiAuth from "@/api/KintaiAuth";
 import RadioButton from "./RadioButton";
+import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
 
 type FormArray = {
   action: string;
@@ -10,11 +13,30 @@ type FormProps = {
   nameLabel: string;
   type: string;
   warningLabel?: string;
+  registerLabel?: string;
+};
+
+type DataProps = {
+  email?: string;
+  password?: string;
 };
 
 export default function From({ action, submitLabel, rows }: FormArray) {
+  const { handleSubmit, register } = useForm();
+
+  const onSubmit = async (data: DataProps) => {
+    try {
+      const res = await KintaiAuth.signInAdmin(data);
+      Cookies.set("access-token", res.headers["access-token"]);
+      Cookies.set("client", res.headers["client"]);
+      Cookies.set("uid", res.headers["uid"]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form action={action} method="post">
+    <form action={action} method="post" onSubmit={handleSubmit(onSubmit)}>
       {rows.map((row, index) => (
         <div className="col-3 mx-auto mt-3" key={index}>
           <label className="form-label">{row.nameLabel}</label>
@@ -22,7 +44,11 @@ export default function From({ action, submitLabel, rows }: FormArray) {
             <RadioButton />
           ) : (
             <>
-              <input type={row.type} className="form-control" />
+              <input
+                type={row.type}
+                className="form-control"
+                {...register(`${row.registerLabel}`)}
+              />
               <div className="form-text">{row.warningLabel}</div>
             </>
           )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,27 @@ import KintaiEmployee from "@/api/KintaiEmployee";
 import Header from "../components/Header";
 import MyHead from "@/components/MyHead";
 import KintaiAdmin from "@/api/KintaiAdmin";
+import Cookies from "js-cookie";
 
 export default function Home() {
+  // 共通で使用するものとしてどこかに置いておきたい
+  const token: any = {
+    headers: {
+      "Content-Type": "application/json",
+      uid: Cookies.get("uid"),
+      client: Cookies.get("client"),
+      "access-token": Cookies.get("access-token"),
+    },
+  };
   // TODO: そのうち削除する。レスポンスを適切に取得できているか確認のためにとりあえず記載。
-
-  // getCurrentAdmin などパラメータにidが含まれていない時にうまくいかない。調査中
   const aaaa = async () => {
-    const hoge = await KintaiAdmin.getAdmin(1);
+    const hoge = await KintaiAdmin.getAdmin(1, token);
     console.log(hoge.data);
   };
   aaaa();
 
   const bbbb = async () => {
-    const fuga = await KintaiEmployee.getShiftForEmployee(1);
+    const fuga = await KintaiEmployee.getCurrentEmployee();
     console.log(fuga.data);
   };
   bbbb();
