5f34a8d9b45714a201e143b0f4a49702ebd74ad3
tatata05 <takumi.takahata@xincere.jp>
2023-12-14 21:01:10 +0900

    shift_calendar

--- src/pages/admin/shifts/index.tsx
+++ src/pages/admin/shifts/index.tsx
@@ -1,28 +1,51 @@
 import AdminHeader from "@/components/AdminHeader";
 import MyHead from "@/components/MyHead";
 import Calendar from "@/components/Calendar";
+import { useEffect, useState } from "react";
+import KintaiAdmin from "@/api/KintaiAdmin";
+import { GetShiftsForAdmin200ResponseDataInner as Shifts } from "@/api/typescript-axios";
 
-// TODO : カレンダー表示内容を取得
-const rows = [
-  {
-    title: "シフト",
-    start: new Date("8 20, 2023 0:00:00"),
-    end: new Date("8 20, 2023 5:00:00"),
-    description: "シフト詳細",
-    backgroundColor: "blue",
-    borderColor: "blue",
-  },
-  {
-    title: "シフト申請",
-    start: new Date("8 25, 2023 2:00:00"),
-    end: new Date("8 25, 2023 4:00:00"),
-    description: "シフト申請",
-    backgroundColor: "blue",
-    borderColor: "blue",
-  },
-];
+type ShiftProps = {
+  rows: RowType[];
+};
+
+type RowType = {
+  title: string;
+  start: Date;
+  end: Date;
+  description: string;
+  backgroundColor: string;
+  borderColor: string;
+};
 
 export default function AdminShifts() {
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [shifts, setShifts] = useState<Shifts[]>([]);
+  const [rows, setRows] = useState<ShiftProps>();
+
+  useEffect(() => {
+    (async () => {
+      const shiftsRes = await KintaiAdmin.getShiftsForAdmin();
+      setShifts(shiftsRes.data.data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const shift_rows: ShiftProps = [];
+    shifts.map((shift) =>
+      shift_rows.push({
+        title: shift.absenceId ? "シフト" : "欠勤",
+        start: new Date(shift.startTime),
+        end: new Date(shift.endTime),
+        description: shift.absenceId ? "シフト詳細" : "欠勤詳細",
+        backgroundColor: shift.absenceId ? "blue" : "red",
+        borderColor: shift.absenceId ? "blue" : "red",
+      }),
+    );
+    setRows(shift_rows);
+    setLoading(false);
+  }, [shifts]);
+
   return (
     <>
       <MyHead title="シフト覧一覧" />
@@ -31,7 +54,7 @@ export default function AdminShifts() {
         <h2 className="mt-5 mb-5">シフト一覧</h2>
         {/* TODO : シフト未申請従業員がいればここに表示 */}
         <div className="mx-5">
-          <Calendar rows={rows} />
+          {isLoading ? <>Loading...</> : <Calendar rows={rows} />}
         </div>
       </main>
     </>
