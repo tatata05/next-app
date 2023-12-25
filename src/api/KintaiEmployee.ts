235f24fc825ebfde71146c75b506b9dc3dd7e73b
tatata05 <takumi.takahata@xincere.jp>
2023-12-25 22:06:08 +0900

    add: axios with credentials

--- src/api/KintaiEmployee.ts
+++ src/api/KintaiEmployee.ts
@@ -2,7 +2,9 @@ import { EmployeeApi } from "@/api/typescript-axios";
 import { Configuration } from "@/api/typescript-axios/configuration";
 import axios from "axios";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  withCredentials: true,
+});
 
 const KintaiEmployee = new EmployeeApi(
   {
